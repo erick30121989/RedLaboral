25c22e4fd22dac7928dadd8be3b96e97630c981a
mcanalsalas <38821800+mcanalsalas@users.noreply.github.com>
2018-07-01 16:44:38 -0500

    prueba

--- RedLaboral/WCF_RedLaboral/Service1.svc.cs
+++ RedLaboral/WCF_RedLaboral/Service1.svc.cs
@@ -10,6 +10,7 @@ namespace WCF_RedLaboral
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE:jijoijoi In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    //prueba
     public class Service1 : IService1
     {
 
