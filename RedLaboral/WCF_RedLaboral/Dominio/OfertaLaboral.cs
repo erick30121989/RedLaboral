6104ed1011f017daaa1137ce85995f9680fdb87d
AirtonHuaman <AirtonHuaman@UL-AHUAMAN>
2018-07-08 16:44:38 -0500

    Servicio OfertaLaboralService, se agregaron métodos de CrearOfertaLaboral y ObtenerOfertaLaboral

--- RedLaboral/WCF_RedLaboral/Dominio/OfertaLaboral.cs
+++ RedLaboral/WCF_RedLaboral/Dominio/OfertaLaboral.cs
@@ -1,11 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace WCF_RedLaboral.Dominio
 {
+    [DataContract]
     public class OfertaLaboral
     {
+        [DataMember]
+        public int idOfertaLaboral { get; set; }
+        [DataMember]
+        public string ruc { get; set; }
+        [DataMember]
+        public string titulo { get; set; }
+        [DataMember]
+        public string lugar { get; set; }
+        [DataMember]
+        public int idPuesto { get; set; }
+        [DataMember]
+        public string descrip { get; set; }
+        [DataMember]
+        public string funciones { get; set; }
+        [DataMember]
+        public int idJornada { get; set; }
+        [DataMember]
+        public int idTipoContrato { get; set; }
+        [DataMember]
+        public string requisitos { get; set; }
+        [DataMember]
+        public string competencias { get; set; }
+        [DataMember]
+        public string estado { get; set; }
     }
 }
